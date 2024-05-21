072d323d83393ed4f9dd3fe22906d3f0fd6c18fb
himanshu <himanshu11199@gmail.com>
2024-05-21 20:50:19 +0530

    changing server file

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,43 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 const session = require('express-session');
-const authRoute = require('./routes/authroutes')
-const adminRoute = require('./routes/adminroutes')
-const dbConnection = require('./model/dbConnection')
-const dotenv = require("dotenv")
+const path = require('path');
+const authRoute = require('./routes/authroutes');
+const adminRoute = require('./routes/adminroutes');
+const dbConnection = require('./model/dbConnection');
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
-dbConnection()
+dbConnection();
 
 app.use(cors({
-    origin: "*"  
-})) 
+    origin: "*"
+}));
 
 app.use(session({
     secret: "I-will-not-tell-you",
-    resave: 'false',
-    saveUninitialized: 'true',
-    // Cookie: {maxage: 40000},
+    resave: false,
+    saveUninitialized: true,
+    // Cookie: {maxAge: 40000},
     // rolling: true
-}))
+}));
 
 app.use(express.json());
 
-// app.get("*",(req,res)=>{
-//     res.sendFile(__dirname+"../client/index.html");
-// })
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use('/', authRoute)
-app.use('/admin', adminRoute )
+app.use('/', authRoute);
+app.use('/admin', adminRoute);
 
+// Catch-all handler to send back `index.html` for any other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log(`server started at port 3000`)
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`);
+});
