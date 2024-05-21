b2274162d55eca014c55abed7bb3d59ea976ac57
himanshu <himanshu11199@gmail.com>
2024-05-21 19:41:06 +0530

    hashed pass

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,11 @@
-
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { HashRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  <HashRouter>
-    <App />
-  </HashRouter>
-  // </React.StrictMode>,
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>,
 )
